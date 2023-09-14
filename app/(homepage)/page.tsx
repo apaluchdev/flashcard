060c09b9ab9cbb0ddb55db31d1c8829ea0f433bf
Adrian Paluch <apaluchdev@gmail.com>
2023-09-13 20:16:20 -0400

    WIP restyling

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -1,45 +1,54 @@
 "use client";
 
-import styles from "./page.module.css";
+import "./styles.css";
 import Link from "next/link";
 import ButtonModal from "@/components/ButtonModal/ButtonModal";
 import AddFlashcard from "@/components/AddFlashcard/AddFlashcard";
 import { IFlashcard } from "@/models/Flashcard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import flashcardClient from "@/lib/flashcard-client";
 import LoadingSpinner from "@/components/LoadingSpinner/LoadingSpinner";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   const router = useRouter();
 
-  const [isLoading, setLoading] = useState<boolean>(false);
+  const [isLoading, setLoading] = useState<boolean>(true);
   const [topic, setTopic] = useState<string>("");
   const [status, setStatus] = useState<string>("");
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
   let newTopicFlashcard: IFlashcard = {
     topic: "",
     question: "",
     answer: "",
   };
 
-  async function GenerateDeck() {
-    setStatus("");
-    setLoading(true);
-    let result = await flashcardClient.GenerateDeck(topic, "medium");
-    setLoading(false);
+  // async function GenerateDeck() {
+  //   setStatus("");
+  //   setLoading(true);
+  //   let result = await flashcardClient.GenerateDeck(topic, "medium");
+  //   setLoading(false);
 
-    if (result.userId && result.topicId)
-      router.push(`/flashcard/${result.userId}/${result.topicId}`);
-    else {
-      setStatus("Failed generation likely due to bad AI response, try again.");
-    }
-  }
+  //   if (result.userId && result.topicId)
+  //     router.push(`/flashcard/${result.userId}/${result.topicId}`);
+  //   else {
+  //     setStatus("Failed generation likely due to bad AI response, try again.");
+  //   }
+  // }
 
   return (
-    <main className={styles.main}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Welcome to Flashcards!</h1>
-        <div className={styles.buttons}>
+    <main className={`flex min-h-screen flex-col items-center justify-center`}>
+      <Title />
+      <div className="mt-12 flex flex-col gap-5 fade-in">
+        <Button>Sign in</Button>
+        <Button>Continue as Guest</Button>
+      </div>
+
+      {/* <div className={styles.buttons}>
           <ButtonModal text="Add Deck">
             <div>
               <AddFlashcard
@@ -51,8 +60,8 @@ export default function Page() {
           <Link className={styles.button} href="/topics">
             Find Decks
           </Link>
-        </div>
-        <hr
+        </div> */}
+      {/* <hr
           style={{
             marginTop: "100px",
             border: "none",
@@ -60,7 +69,9 @@ export default function Page() {
             height: "1px",
             backgroundColor: "black",
           }}
-        ></hr>
+        >
+          
+        </hr>
         <div className={styles.generate}>
           <h1>Generate a deck using AI</h1>
           <p className={styles.preview}>! Work in progess preview !</p>
@@ -79,8 +90,25 @@ export default function Page() {
               {status}
             </div>
           </div>
-        </div>
-      </div>
+        </div> */}
     </main>
   );
 }
+
+export const Title = () => {
+  return (
+    <div className="mb-12">
+      <h1 className="text-center text-8xl tracking-tighter text-gray-700 fade-in">
+        Flashcards
+      </h1>
+      <h1 className=" text-7xl tracking-tighter text-gray-500">
+        <span className="fade-in-delay-1 text-theme-3">Study</span>
+        <span className="fade-in-delay-1 text-6xl">. </span>
+        <span className="fade-in-delay-2 text-theme-2">Flip</span>
+        <span className="fade-in-delay-2 text-6xl">. </span>
+        <span className="fade-in-delay-3 text-theme-1">Learn</span>
+        <span className="fade-in-delay-3 text-6xl">. </span>
+      </h1>
+    </div>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import "./globals.css";
-import { Inter, Open_Sans, Quicksand } from "next/font/google";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Inter, Open_Sans, Quicksand, Roboto } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 
 const openSans = Open_Sans({ subsets: ["latin"], weight: "300" });
+const inter = Inter({ subsets: ["latin"], weight: "800" });
+const roboto = Roboto({ subsets: ["latin"], weight: "300" });
 
 export const metadata = {
   title: "Flashcards",
@@ -18,10 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={openSans.className}>
-        <Header />
+      {/* <body className={`${quicksand.className} text-red-500`}> */}
+      <body className={`${inter.className}`}>
+        {/* <Header /> */}
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
