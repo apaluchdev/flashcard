75d5a78631f1ab63c638b857803ce0bc8e681682
Adrian Paluch <apaluchdev@gmail.com>
2023-09-25 00:37:07 -0400

    endpoint testing on prod

--- app/api/GetTopics2/route.ts
+++ app/api/GetTopics2/route.ts
@@ -5,9 +5,9 @@ import Topic, { ITopic } from "@/models/Topic";
 export async function GET(req: NextRequest): Promise<NextResponse> {
   try {
     console.log("Finding topics!");
-    const topics: ITopic[] = await Topic.find({ userId: "Adrian" });
-    console.log(`Found topics: ${topics}`);
-    return NextResponse.json({ topics }, { status: 200 });
+    // const topics: ITopic[] = await Topic.find({ userId: "Adrian" });
+    // console.log(`Found topics: ${topics}`);
+    return NextResponse.json({ test: "success" }, { status: 200 });
   } catch (error) {
     console.log(`Error in API GET for topics ${error}`);
     return NextResponse.json({ msg: "Error fetching topic" }, { status: 500 });
