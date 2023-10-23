d0a440f80508f002ad308a9656903af974ff5f35
Adrian Paluch <apaluchdev@gmail.com>
2023-10-23 00:06:17 -0400

    Fixed the quill bug

--- components/flashcard-card/cardface.tsx
+++ components/flashcard-card/cardface.tsx
@@ -15,14 +15,14 @@ interface CardFaceProps {
 
 const CardFace: React.FC<CardFaceProps> = ({ question, answer, topic }) => {
   return (
-    <div>
+    <div className="w-full">
       <CardHeader className="text-center">
         <CardDescription>{decodeURIComponent(topic)}</CardDescription>
         <CardTitle className="whitespace-normal break-words text-xl md:text-4xl">
           {question}
         </CardTitle>
       </CardHeader>
-      <CardContent>
+      <CardContent className="w-full">
         <TextViewer text={answer} />
       </CardContent>
       <CardFooter></CardFooter>
--- components/text-editor/text-editor.tsx
+++ components/text-editor/text-editor.tsx
@@ -5,7 +5,6 @@ import styles from "./text-editor.module.css";
 import dynamic from "next/dynamic";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import "react-quill/dist/quill.snow.css";
-import EditorToolbar, { formats, modules } from "./editor-toolbar";
 
 interface Props {
   TextEditorCallback: Function;
@@ -19,40 +18,31 @@ const TextEditor: React.FC<Props> = ({ TextEditorCallback, initialText }) => {
     TextEditorCallback(value);
   }, [value]);
 
-  var toolbarOptions = [
-    ["bold", "italic", "underline", "strike"], // toggled buttons
-    ["blockquote", "code-block"],
-
-    [{ header: 1 }, { header: 2 }], // custom button values
-
-    ["clean"], // remove formatting button
-  ];
-
-  // const modules = {
-  //   toolbar: {
-  //     toolbarOptions,
-  //   },
-  // };
+  let modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "code-block"],
+      ["clean"],
+    ],
+  };
 
   return (
     <div className={styles.text}>
-      <EditorToolbar />
       <ReactQuill
         theme="snow"
         value={value}
         onChange={setValue}
         placeholder={"Enter answer..."}
         modules={modules}
-        formats={formats}
       />
     </div>
-
-    // <ReactQuill
-    //   theme="snow"
-    //   className="h-full text-4xl"
-    //   value={value}
-    //   onChange={setValue}
-    // />
   );
 };
 
