b0c4bec74eec7d956b8df96d477bb621aa4013c8
apaluchdev <apaluchdev@gmail.com>
2024-07-21 22:18:29 -0400

    Testing with caching removed

--- next.config.js
+++ next.config.js
@@ -2,19 +2,19 @@ const mongoose = require("mongoose");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/GetTopics",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, max-age=0",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/api/GetTopics",
+  //       headers: [
+  //         {
+  //           key: "Cache-Control",
+  //           value: "no-store, max-age=0",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 if (mongoose.connection.readyState === 1) {
