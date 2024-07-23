8e6883e7dd741161050e26eaf8ae20ec6ac9d751
apaluchdev <apaluchdev@gmail.com>
2024-07-23 19:05:57 -0400

    more testing

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -9,6 +9,8 @@ export default async function Page() {
 
   if (!topics) throw new Error("Could not fetch topics in server code hmm");
 
+  const myTopic = await Topic.findOne({ topicTitle: "CompTIA Security  1.5" });
+
   console.log("Found topics: ", topics);
   return (
     <div className="flex flex-col items-center justify-center">
@@ -16,6 +18,7 @@ export default async function Page() {
         topics.map((topic) => (
           <div key={topic.topicTitle}>{topic.topicTitle}</div>
         ))}
+      <h1>Special request: {myTopic?.topicTitle}</h1>
     </div>
   );
 }
